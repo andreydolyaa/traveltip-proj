d3fdc778e89ebfd7ea1e5ce3708f285c1a3205b3
Andrey Dolya <dolya7kk@gmail.com>
2020-10-28 19:03:40 +0200

    weather rendering

--- js/weather-controller.js
+++ js/weather-controller.js
@@ -2,7 +2,7 @@
 
 import { weatherService } from '/js/weather-service.js';
 
-// onInit();
+onInit();
 
 function onInit() {
     var weather = weatherService.loadWeather('tel-aviv')
@@ -20,6 +20,6 @@ function renderWeather(res) {
     <div><img src="http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png"></div>
     <h2>${res.name}, ${res.sys.country}, ${res.weather[0].description}</h2>
     <h1><span>${res.main.temp.toFixed(1)}c</span></h1>
-    <p>temperature from ${res.main.temp_min} to ${res.main.temp_max}c wind ${res.wind.speed} m/s </p> 
+    <p>temperature from ${res.main.temp_min} to ${res.main.temp_max}c, wind ${res.wind.speed} m/s </p> 
     `;
 }
