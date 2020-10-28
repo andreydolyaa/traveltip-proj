9522ed18baf03a18b84b9fb36b94f45f4f1ae707
Andrey Dolya <dolya7kk@gmail.com>
2020-10-28 17:19:48 +0200

    sdfsd

--- js/weather-service.js
+++ js/weather-service.js
@@ -1,11 +1,34 @@
 'use strict';
 
+const STORAGE_KEY = 'weatherDB';
+var gWeather;
 
-loadWeather()
 
-function loadWeather(){
-    return axios.get('http://api.openweathermap.org/data/2.5/forecast?id=0581774118db611eb667beb10ab2fcdd')
-    .then(res => {
-        console.log(res.data);
-    })
+
+loadWeather();
+
+
+function loadWeather() {
+    var weather = loadFromStorage(STORAGE_KEY);
+    if (weather && weather.length) {
+        gWeather = weather;
+        _saveToStorage();
+        console.log('loading weather from local storage');
+        return gWeather;
+    }
+    return axios.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=0581774118db611eb667beb10ab2fcdd')
+        .then(res => {
+            console.log(res.data.list);
+            gWeather = res.data.list;
+            _saveToStorage();
+            console.log('loading weather from server');
+            return res.data.list;
+        })
+}
+
+
+
+
+function _saveToStorage() {
+    saveToStorage(STORAGE_KEY, gWeather);
 }
