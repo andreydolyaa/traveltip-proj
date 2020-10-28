a1a1e430368ca7721aa4d16f31a93be66f53049d
harelwn <harelwn@users.noreply.github.com>
2020-10-28 20:28:38 +0200

    EMET

--- js/mapController.js
+++ js/mapController.js
@@ -62,8 +62,8 @@ export function showLocationFromInput(res){
     console.log('event showLocation', res); 
     //console.log(event.coords.latitude); 
     //console.log(event.coords.longitude);
-    var lat = res.coords.lon;
-    var lng = res.coords.lat; 
+    var lat = res.coord.lon;
+    var lng = res.coord.lat; 
     panTo(lat, lng);
     placeMarkerMyLocation(lat, lng);
 } 
